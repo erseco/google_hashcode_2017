03e9da1a330e0558730a4057b59d2dd29cd759ad
BraulioV <brauliovarlop@correo.ugr.es>
2017-02-13 17:57:31 +0100

    comments

--- Part 1/pizza.py
+++ Part 1/pizza.py
@@ -18,9 +18,9 @@ class Pizza:
     def load_file(self, file_name):
 
         with open(file_name) as file:
-
+            # Read the parameters of the pizza
             header = file.readline().split(' ')
-
+            # and set up the pizza
             self.number_of_rows = int(header[0])
             self.number_of_columns = int(header[1])
             self.minimum_of_each_ingredient_per_slice = int(header[2])
@@ -29,7 +29,7 @@ class Pizza:
             self.matrix = np.zeros((self.number_of_rows, self.number_of_columns), dtype=np.character)
 
             i = 0
-
+            # Load the pizza
             for line in file:
                 a = np.array(list(line)[0:len(line)-1], dtype=np.character)
                 self.matrix[i] = a
